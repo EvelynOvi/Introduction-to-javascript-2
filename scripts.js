11c441df8f1d13e378f9ae53038172597b86098d
Evelyn Oviangbede <103006429+EvelynOvi@users.noreply.github.com>
2022-06-24 11:38:37 +0100

    validation update

--- scripts.js
+++ scripts.js
@@ -1,78 +1,40 @@
-window.addEventListener("load", () => {
-    Operate();
-})
+const isNumber = (number) => {
+    if (isNaN(number))
+        return isNumber(prompt("Input not a number, please enter a number: "))
 
-function Operate() {
-    let operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
-
-    if (operator === null) {
-        return;
-    }
-
-    while (!['+','-','*','/'].includes(operator)) {
-        operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
-       
-
-        if(operator === null){
-            return;
-        }
-        
-    }
-
-   
-
-    const isNumber = (number) => {
-        if (isNaN(number)) {
-            return false;
-        }
-        return true;
-    }
-
-    let inputtedFirstNumber = prompt("Enter the first number");
-    if (inputtedFirstNumber === null) {
-        return
-    }
-    while (!isNumber(inputtedFirstNumber)) {
-        inputtedFirstNumber = prompt("Input a valid first number");
-        if (inputtedFirstNumber === null) {
-            return;
-        }
-    }
-
-
-
-    let inputtedSecondNumber = prompt("Enter the second number");
-    if (inputtedSecondNumber === null) {
-        return;
-    }
-
-    while (!isNumber(inputtedSecondNumber)) {
-        inputtedSecondNumber = prompt("Input a valid second number");
-        if (inputtedSecondNumber === null) {
-            return;
-        }
-    }
-
-    const num1 = parseFloat(inputtedFirstNumber);
-    const num2 = parseFloat(inputtedSecondNumber);
-
-
-
-    let result; // declaration of the variable.
-
-    if (operator == '+') {
-        result = num1 + num2;
-    }
-    else if (operator == '-') {
-        result = num1 - num2;
-    }
-    else if (operator == '*') {
-        result = num1 * num2;
-    }
-    else if (operator == '/') {
-        result = num1 / num2;
-    }
-
-
-    window.alert(" Your operation: " + num1 + ' ' + operator + ' ' + num2 + ' ' + '= '  + result);
+    return parseFloat(number)
 }
+
+
+
+const isOperator = (op) => {
+    if (!['+', '-', '*', '/'].includes(op))
+        return isOperator(prompt("Please enter a valid operator( either +, -, * or / ): "))
+    return op
+}
+
+
+const operator = isOperator(prompt('Enter operator to perform the calculation ( either +, -, * or / ): '));
+
+const num1 = isNumber(prompt("Input first number: "));
+
+const num2 = isNumber(prompt("Input second number: "));
+
+
+let result; // declaration of the variable.
+
+if (operator == '+') {
+    result = num1 + num2;
+}
+else if (operator == '-') {
+    result = num1 - num2;
+}
+else if (operator == '*') {
+    result = num1 * num2;
+}
+else if (operator == '/') {
+    result = num1 / num2;
+}
+
+
+window.alert(" Your operation: " + num1 + ' ' + operator + ' ' + num2 + ' ' + '= ' + result);
